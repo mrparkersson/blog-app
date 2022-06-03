004e2a71e520d4a81d2ca57e71fcd9b49f2e6790
mrparkersson <amoparkerprince78@gmail.com>
2022-06-03 15:39:17 +0000

    Rename fields to use snake case in post

--- db/migrate/20220603135630_create_posts.rb
+++ db/migrate/20220603135630_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :title
       t.string :text
       t.integer :comments_counter
-      t.integer :likescounter
+      t.integer :likes_counter
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
