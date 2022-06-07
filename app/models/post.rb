3c41665fa1d4118581b8af328feaf253f8056eea
mrparkersson <amoparkerprince78@gmail.com>
2022-06-07 06:48:27 +0000

    Create data for all models in database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def update_posts_counter
+    author.increment!(:PostsCounter)
+  end
+
+  def return_five_most_recent_comments
+    comments.order('created_at DESC').limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def most_recent_post
+    Post.order('created_at DESC').where(author_id: id).limit(3)
+  end
 end
