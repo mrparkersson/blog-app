b0f5ccd1baf0d2520c858d08227214b8a14bd6d3
mrparkersson <amoparkerprince78@gmail.com>
2022-06-08 09:18:14 +0000

    Set display for users posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ActionController::Base
   def index
-    puts params
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments).order(created_at: :DESC).page(params[:page]).per(5)
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
