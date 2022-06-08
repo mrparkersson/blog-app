1923947a3a55ba8b5af21c60d3a29f3baf9cd1e0
mrparkersson <amoparkerprince78@gmail.com>
2022-06-08 09:43:56 +0000

    add styles for blog app views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ActionController::Base
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments).order(created_at: :DESC).page(params[:page]).per(5)
   end
 
   def show
