9420342fd344731160417ee23cd2c92aa59b34c6
nevisende <ekonomitakipmail@gmail.com>
2022-06-16 23:52:16 +0300

    Feat: create authenticate_request method in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  include JsonWebToken
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_request
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    header = header.split.last if header
+    decoded = jwt_decode(header)
+    @curr_user = User.find(decoded[:user_id])
+  end
 
   protected
 
