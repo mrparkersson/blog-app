e00e45e8885a495cbda41c9811c4074d6c34216f
mrparkersson <amoparkerprince78@gmail.com>
2022-06-17 07:32:45 +0000

    Describe each user in user spec

--- spec/integration/user_show_page_spec.rb
+++ spec/integration/user_show_page_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe 'Login', type: :feature do
   describe 'User' do
     before(:each) do
       @user1 = User.create!(email: 'parker@gmail.com', password: 'abcdef',
-        name: 'parker', bio: 'parker\'s bio...',
-        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')
+                            name: 'parker', bio: 'parker\'s bio...',
+                            photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')
 
-visit '/users/sign_in'
-fill_in 'Email', with: 'furkan@gmail.com'
-fill_in 'Password', with: '123456'
-click_button 'Log in'
+      visit '/users/sign_in'
+      fill_in 'Email', with: 'furkan@gmail.com'
+      fill_in 'Password', with: '123456'
+      click_button 'Log in'
 
-@post1 = @user1.posts.create!(title: 'test_1', text: 'Test cases 1', comments_counter: 0, likes_counter: 0,
-  user_id: @user1.id)
-@post2 = @user1.posts.create!(title: 'test_2', text: 'Test cases 2', comments_counter: 0, likes_counter: 0,
-  user_id: @user1.id)
-@post3 = @user1.posts.create!(title: 'test_3', text: 'Test cases 3', comments_counter: 0, likes_counter: 0,
-  user_id: @user1.id)
+      @post1 = @user1.posts.create!(title: 'test_1', text: 'Test cases 1', comments_counter: 0, likes_counter: 0,
+                                    user_id: @user1.id)
+      @post2 = @user1.posts.create!(title: 'test_2', text: 'Test cases 2', comments_counter: 0, likes_counter: 0,
+                                    user_id: @user1.id)
+      @post3 = @user1.posts.create!(title: 'test_3', text: 'Test cases 3', comments_counter: 0, likes_counter: 0,
+                                    user_id: @user1.id)
 
-  visit user_posts_path(@user1)
+      visit user_posts_path(@user1)
     end
 
     it 'shows photo' do
